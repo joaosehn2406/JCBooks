dbe9aa93b7acb686533ace115a67540fba5b7fa0
Joao Pedro Sehnem dos Santos <jpssantosos5874@gmail.com>
2025-05-23 16:25:21 -0300

    feat: simplified variables and methods on favoriteFragment and homeFragment

--- app/src/main/java/com/jceco/jcbooks/ui/FavoriteFragment.kt
+++ app/src/main/java/com/jceco/jcbooks/ui/FavoriteFragment.kt
@@ -6,31 +6,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.jceco.jcbooks.databinding.FragmentDashboardBinding
-import com.jceco.jcbooks.viewmodels.DashboardViewModel
+import com.jceco.jcbooks.viewmodels.FavoriteViewModel
 
-class DashboardFragment : Fragment() {
+class FavoriteFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val viewModel: FavoriteViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        viewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
