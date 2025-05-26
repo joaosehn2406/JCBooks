ec1a96e96f296a2a2e837bb17dc308f7400999da
joaosehn2406 <jpssantosos5874@gmail.com>
2025-05-26 11:27:29 -0300

    chore: created RecyclerView in fragment_home and partial implementations in HomeFragment

--- app/src/main/java/com/jceco/jcbooks/ui/HomeFragment.kt
+++ app/src/main/java/com/jceco/jcbooks/ui/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jceco.jcbooks.databinding.FragmentHomeBinding
 import com.jceco.jcbooks.viewmodels.HomeViewModel
 
@@ -22,16 +23,13 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        viewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        binding.recyclerviewBooks.layoutManager = LinearLayoutManager(context)
+
+
+
+        return binding.root
     }
 
     override fun onDestroyView() {
