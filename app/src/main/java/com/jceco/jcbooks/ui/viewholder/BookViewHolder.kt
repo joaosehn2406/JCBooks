b87afc17a8bdc96273c991674d83b70da2d5d7a6
joaosehn2406 <jpssantosos5874@gmail.com>
2025-05-26 15:49:23 -0300

    Commit to unbug git

--- app/src/main/java/com/jceco/jcbooks/ui/viewholder/BookViewHolder.kt
+++ app/src/main/java/com/jceco/jcbooks/ui/viewholder/BookViewHolder.kt
@@ -14,7 +14,7 @@ class BookViewHolder(private val item: ItemBookBinding) : RecyclerView.ViewHolde
 
         defineBackground(book.genre)
 
-        updateFavoriteIcon(book.favorite)
+        updateFavoriteIcon(book.author)
     }
 
     private fun defineBackground(genre: String) = when (genre) {
