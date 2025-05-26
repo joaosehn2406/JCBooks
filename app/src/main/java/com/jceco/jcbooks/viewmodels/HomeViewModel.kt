30321da7370fa07de510d9501b93730cbef9388b
joaosehn2406 <jpssantosos5874@gmail.com>
2025-05-26 13:24:06 -0300

    refactor: HomeViewModel partially refactored to comunicate better with all other classes

--- app/src/main/java/com/jceco/jcbooks/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/jceco/jcbooks/viewmodels/HomeViewModel.kt
@@ -3,18 +3,17 @@ package com.jceco.jcbooks.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.jceco.jcbooks.entity.Book
 import com.jceco.jcbooks.repository.BookRepository
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
+    private val _books = MutableLiveData<List<Book>>()
+    val books: LiveData<List<Book>> = _books
 
     private val respository = BookRepository()
 
     fun getAllBooks() {
-        respository.getAllBooks()
+        _books.value = respository.getAllBooks()
     }
 }
