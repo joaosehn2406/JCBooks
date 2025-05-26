86f8c433a97d628ab87ef78e33e73899a35e8eb0
joaosehn2406 <jpssantosos5874@gmail.com>
2025-05-26 11:34:26 -0300

    feat: created BookViewHolder and item_book to use in adapter

---
+++ app/src/main/java/com/jceco/jcbooks/ui/viewholder/BookViewHolder.kt
@@ -0,0 +1,7 @@
+package com.jceco.jcbooks.ui.viewholder
+
+import androidx.recyclerview.widget.RecyclerView
+import com.jceco.jcbooks.databinding.ItemBookBinding
+
+class BookViewHolder(private val item: ItemBookBinding) : RecyclerView.ViewHolder(item.root) {
+}
