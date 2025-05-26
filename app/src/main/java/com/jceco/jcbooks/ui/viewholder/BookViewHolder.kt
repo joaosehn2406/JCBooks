5291f013dfb7e4c8e8ecc89a8063aba9f4a94e5f
joaosehn2406 <jpssantosos5874@gmail.com>
2025-05-26 15:45:39 -0300

    feat: defineBackground and updateFavoriteIcon methods created to make the drawable icons dinamic

--- app/src/main/java/com/jceco/jcbooks/ui/viewholder/BookViewHolder.kt
+++ app/src/main/java/com/jceco/jcbooks/ui/viewholder/BookViewHolder.kt
@@ -1,6 +1,7 @@
 package com.jceco.jcbooks.ui.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
+import com.jceco.jcbooks.R
 import com.jceco.jcbooks.databinding.ItemBookBinding
 import com.jceco.jcbooks.entity.Book
 
@@ -10,5 +11,26 @@ class BookViewHolder(private val item: ItemBookBinding) : RecyclerView.ViewHolde
         item.textviewTitle.text = book.title
         item.textviewGenre.text = book.genre
         item.textviewAuthor.text = book.author
+
+        defineBackground(book.genre)
+
+        updateFavoriteIcon()
+    }
+
+    private fun defineBackground(genre: String) = when (genre) {
+        "Terror" -> {
+            item.textviewGenre.setBackgroundResource(R.drawable.rounded_label_red)
+        }
+        "Fantasia" -> {
+            item.textviewGenre.setBackgroundResource(R.drawable.rounded_label_fantasy)
+        }
+        else -> {
+            item.textviewGenre.setBackgroundResource(R.drawable.rounded_label_teal)
+        }
+    }
+
+    private fun updateFavoriteIcon(favorite: Boolean) = when (favorite){
+        true -> item.imageviewFavorite.setImageResource(R.drawable.ic_favorite)
+        false -> item.imageviewFavorite.setImageResource(R.drawable.ic_favorite_empty)
     }
 }
