710ea8583c23646f499469f651ab03b35fcf4923
joaosehn2406 <jpssantosos5874@gmail.com>
2025-05-27 08:55:05 -0300

    feat: added an user alert for book removal

--- app/src/main/java/com/jceco/jcbooks/ui/DetailsFragment.kt
+++ app/src/main/java/com/jceco/jcbooks/ui/DetailsFragment.kt
@@ -1,9 +1,12 @@
 package com.jceco.jcbooks.ui
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.jceco.jcbooks.R
@@ -46,6 +49,18 @@ class DetailsFragment : Fragment() {
             binding.checkboxFavorito.isChecked = it.favorite
 
             defineBackground(it.genre)
+
+            viewModel.bookRemoval.observe(viewLifecycleOwner) {
+                if (it) {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.msg_removed_successfully),
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    requireActivity().supportFragmentManager.popBackStack()
+                }
+            }
         }
     }
 
@@ -53,15 +68,39 @@ class DetailsFragment : Fragment() {
         binding.imageviewBack.setOnClickListener {
             requireActivity().supportFragmentManager.popBackStack()
         }
+
+        binding.buttonRemove.setOnClickListener { handleRemove() }
+
+        binding.checkboxFavorito.setOnClickListener { handleFavorite() }
     }
+
+    private fun handleFavorite() {
+        viewModel.favorite(bookId)
+
+    }
+
+    private fun handleRemove() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setMessage("Tem certeza que deseja excluir esse item?")
+            .setPositiveButton("SIM") { dialog, which ->
+                viewModel.deleteBook(bookId)
+            }
+            .setNegativeButton("NÃO") { dialog, which ->
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
+
 
     private fun defineBackground(genre: String) = when (genre) {
         "Terror" -> {
             binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_red)
         }
+
         "Fantasia" -> {
             binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_fantasy)
         }
+
         else -> {
             binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_teal)
         }
